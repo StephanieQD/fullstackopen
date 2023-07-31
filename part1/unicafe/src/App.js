3718e3f033d594efb153bb364bfd4f40c93afc65
StephanieQD <stephanie.qrys.deetman@gmail.com>
2023-07-31 14:25:33 -0500

    Complete exercise 1.8 / Unicafe step3

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,6 +4,26 @@ const Button = ({handleClick, text}) => (
   <button className={`button ${text}`} onClick={handleClick}>{text}</button>
 )
 
+const Statistics = ({good, bad, neutral}) => {
+  const total = good + neutral + bad;
+
+  const calcAvg = () => total !== 0 ? (good + (bad * -1)) / total : 'N/A';
+  const calcPos = () => total !== 0 ? (good / total) * 100 : 'N/A';
+
+  return (
+    <div>
+      <h2>Statistics</h2>
+      good {good} <br />
+      neutral {neutral} <br />
+      bad {bad} <br />
+      all {total} <br />
+      average {calcAvg()}<br />
+      positive {calcPos()}%
+    </div>
+    
+  );
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -14,24 +34,13 @@ const App = () => {
   const incrementNeutral = () => setNeutral(neutral + 1)
   const incrementBad = () => setBad(bad + 1)
 
-  const total = good + neutral + bad;
-
-  const calcAvg = () => total !== 0 ? (good + (bad * -1)) / total : 'N/A';
-  const calcPos = () => total !== 0 ? (good / total) * 100 : 'N/A';
-
   return (
     <main>
       <h1>Give Feedback</h1>
       <Button text="good" handleClick={incrementGood}/>
       <Button text="neutral" handleClick={incrementNeutral}/>
       <Button text="bad" handleClick={incrementBad}/>
-      <h2>Statistics</h2>
-      good {good} <br />
-      neutral {neutral} <br />
-      bad {bad} <br />
-      all {total} <br />
-      average {calcAvg()}<br />
-      positive {calcPos()}%
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </main>
   )
 }
