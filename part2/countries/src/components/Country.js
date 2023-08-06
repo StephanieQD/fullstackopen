a187b043c6363d9c57fc01a32ec70896da51ec41
StephanieQD <stephanie.qrys.deetman@gmail.com>
2023-08-05 20:05:30 -0500

    Complete exercise 2.20*: Data for countries, step3
    "Add to the view showing the data of a single country, the weather report for the capital of that country"

--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Country = ({country}) => {
+  const [weather, setWeather] = useState([])
+
+  useEffect(() => {
+    const params = {
+      APPID: process.env.REACT_APP_API_KEY,
+      q: country.capital[0],
+      units: 'metric'
+    }
+
+    axios.get('https://api.openweathermap.org/data/2.5/weather', {params})
+      .then(response => {
+        const apiResponse = response.data;
+        console.log(apiResponse)
+        setWeather([apiResponse])
+      })
+      .catch(error => {
+        console.log(error);
+      })
+    }, [])
+
   console.log(country)
   return (
     <div className="country-info">
@@ -18,6 +39,15 @@ const Country = ({country}) => {
       }
       </ul>
       <img src={country.flags.png} alt={`Flag of ${country.name.common}`} />
+      { weather.length > 0 && 
+        <>
+          <h3>Weather in {weather[0].name}</h3>
+          <p><b>Temperature:</b> {weather[0].main.temp}° C</p>
+          <img src={`https://openweathermap.org/img/wn/${weather[0].weather[0].icon}@2x.png`} alt={weather[0].weather[0].description} />
+          <br /><i>{weather[0].weather[0].description} </i>
+          <p><b>Wind:</b> {weather[0].wind.speed} m/s</p>
+        </>
+      }
     </div>
   )
 }
