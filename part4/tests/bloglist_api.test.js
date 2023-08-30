8a33e701dbb3cc69bd484260751c79b3c59a372f
StephanieQD <stephanie.qrys.deetman@gmail.com>
2023-08-30 16:25:18 -0500

    Complete exercise 4.11*: Blog list tests, step4
    Write a test that verifies that if the likes property is missing from the request, it will default to the value 0.

--- part4/tests/bloglist_api.test.js
+++ part4/tests/bloglist_api.test.js
@@ -4,6 +4,19 @@ const supertest = require('supertest')
 const app = require('../app')
 
 const api = supertest(app)
+const Blog = require('../models/blog')
+const logger = require('../utils/logger')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  logger.info('Deleting all...')
+
+  const blogObjects = helper.initialBlogs
+    .map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
 
 test('blogs are returned as json', async () => {
   await api
@@ -35,12 +48,34 @@ describe('addition of a new blog', () => {
       .expect('Content-Type', /application\/json/)
 
     const blogsAtEnd = await helper.blogsInDb()
-    expect(blogsAtEnd).toHaveLength(1)
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
 
     const contents = blogsAtEnd.map(n => n.title)
     expect(contents).toContain(
       'My daddy\'s dead :('
     )
+  })
+})
+
+describe('addition of a new blog without a likes property', () => {
+  test('succeeds with valid data and likes unset', async () => {
+    const newBlog = {
+      title: 'Example Blog',
+      author: 'Sample Person',
+      url: 'https://fullstackopen.com/en/part4/testing_the_backend#exercises-4-8-4-12',
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+    logger.info('!!! Blogs !!!')
+    logger.info(blogsAtEnd)
   })
 })
 
