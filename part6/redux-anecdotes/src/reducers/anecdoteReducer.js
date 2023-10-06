b26359569bf027250f5e589a040150f9402a8d31
StephanieQD <stephanie.qrys.deetman@gmail.com>
2023-10-06 13:52:22 -0500

    Complete 6.16 Anecdotes and the backend, step3
    "Modify the initialization of the Redux store to happen using asynchronous action creators, which are made possible by the Redux Thunk library."

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,5 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+import anecdoteService from '../services/anecdotes'
+
 const anecdoteSlice = createSlice({
   name: 'anecdote',
   initialState: [], 
@@ -31,5 +33,12 @@ const anecdoteSlice = createSlice({
 })
 
 export const { createAnecdote, increaseVote, appendAnecdote, setAnecdotes } = anecdoteSlice.actions
+
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch(setAnecdotes(anecdotes))
+  }
+}
 
 export default anecdoteSlice.reducer;
