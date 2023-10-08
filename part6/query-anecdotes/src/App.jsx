32c18be442cf94be84a1666a3d03d2d2fa2c6ce6
StephanieQD <stephanie.qrys.deetman@gmail.com>
2023-10-08 15:15:43 -0500

    Complete Exercise 6.20
    "Implement retrieving anecdotes from the server using React Query."

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,19 +1,30 @@
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
 const App = () => {
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: () => axios.get('http://localhost:3001/anecdotes').then(res => res.data),
+    retry: 1
+  })
+  console.log(JSON.parse(JSON.stringify(result)))
+
+  if ( result.isLoading ) {
+    return <div>loading data...</div>
+  }
+
+  if ( result.isError ) {
+    return <div><p>anecdote service not available due to problems in server.</p></div>
+  }
+
+  const anecdotes = result.data
 
   const handleVote = (anecdote) => {
     console.log('vote')
   }
-
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
 
   return (
     <div>
