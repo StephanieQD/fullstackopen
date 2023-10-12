8c3ddc309a1909f7e4cd2d96a1b420ffa0342ae8
StephanieQD <stephanie.qrys.deetman@gmail.com>
2023-10-12 18:56:38 -0500

    Complete exercise 7.4: anecdotes and hooks step1

---
+++ part7/routed-anecdotes/src/components/CreateNew.jsx
@@ -0,0 +1,45 @@
+import { useNavigate } from 'react-router-dom'
+import  { useField } from '../hooks'
+
+const CreateNew = (props) => {
+  const navigate = useNavigate()
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    props.addNew({
+      content: content.value,
+      author: author.value,
+      info: info.value,
+      votes: 0
+    })
+
+    navigate('/') // Redirect to overview page after submission.
+  }
+
+  return (
+    <div>
+      <h2>create a new anecdote</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          content
+          <input name='content' {...content} />
+        </div>
+        <div>
+          author
+          <input name='author' {...author} />
+        </div>
+        <div>
+          url for more info
+          <input name='info' {...info} />
+        </div>
+        <button>create</button>
+      </form>
+    </div>
+  )
+}
+
+export default CreateNew
