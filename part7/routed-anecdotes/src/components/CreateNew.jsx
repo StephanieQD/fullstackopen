cbd698310aa8561f8dcc41c286cdcfc90147b661
StephanieQD <stephanie.qrys.deetman@gmail.com>
2023-10-13 14:26:39 -0500

    Complete exercise 7.6: anecdotes and hooks step3

--- part7/routed-anecdotes/src/components/CreateNew.jsx
+++ part7/routed-anecdotes/src/components/CreateNew.jsx
@@ -11,9 +11,9 @@ const CreateNew = (props) => {
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content: content.value,
-      author: author.value,
-      info: info.value,
+      content: content.input.value,
+      author: author.input.value,
+      info: info.input.value,
       votes: 0
     })
 
@@ -33,15 +33,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' {...content} />
+          <input name='content' {...content.input} />
         </div>
         <div>
           author
-          <input name='author' {...author} />
+          <input name='author' {...author.input} />
         </div>
         <div>
           url for more info
-          <input name='info' {...info} />
+          <input name='info' {...info.input} />
         </div>
         <button>create</button>
         <button onClick={handleReset}>reset</button>
--- part7/routed-anecdotes/src/hooks/index.js
+++ part7/routed-anecdotes/src/hooks/index.js
@@ -12,9 +12,12 @@ export const useField = (type) => {
   }
 
   return {
-    type,
-    value,
-    onChange,
+    input:
+      {
+        type,
+        value,
+        onChange
+      },
     reset
   }
 }
