efdf342bded657fe4d99a357f7c03fbf929dafce
StephanieQD <stephanie.qrys.deetman@gmail.com>
2023-10-25 19:18:51 -0500

    Complete exercise 7.11: React Query and context step2

--- part7/blogapp/frontend/src/App.js
+++ part7/blogapp/frontend/src/App.js
@@ -10,8 +10,25 @@ import Notification from './components/Notification'
 import Togglable from './components/Togglable'
 import { useNotify } from './components/NotificationContext'
 
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const queryClient = useQueryClient()
+  const result = useQuery({
+    queryKey: ['blogs'],
+    queryFn: blogService.getAll,
+    refetchOnWindowFocus: false,
+    retry: 1,
+  })
+
+  const newBlogMutation = useMutation({
+    mutationFn: blogService.create,
+    onSuccess: (newBlog) => {
+      const blogs = queryClient.getQueryData(['blogs'])
+      queryClient.setQueryData(['blogs'], blogs.concat(newBlog))
+    },
+  })
+
   const [user, setUser] = useState('')
 
   const blogFormRef = useRef()
@@ -21,11 +38,6 @@ const App = () => {
   useEffect(() => {
     const user = storageService.loadUser()
     setUser(user)
-  }, [])
-
-  useEffect(() => {
-    blogService.getAll().then((blogs) => setBlogs(blogs))
-    notifyWith('all blogs loaded')
   }, [])
 
   const notifyWith = (message, type = 'info') => {
@@ -54,17 +66,17 @@ const App = () => {
   }
 
   const createBlog = async (newBlog) => {
-    const createdBlog = await blogService.create(newBlog)
+    newBlogMutation.mutate(newBlog)
     notifyWith(`A new blog '${newBlog.title}' by '${newBlog.author}' added`)
-    setBlogs(blogs.concat(createdBlog))
     blogFormRef.current.toggleVisibility()
   }
 
   const like = async (blog) => {
     const blogToUpdate = { ...blog, likes: blog.likes + 1, user: blog.user.id }
     const updatedBlog = await blogService.update(blogToUpdate)
+    console.log(updatedBlog) // Get rid of "'updatedBlog' is assigned a value but never used" error
     notifyWith(`A like for the blog '${blog.title}' by '${blog.author}'`)
-    setBlogs(blogs.map((b) => (b.id === blog.id ? updatedBlog : b)))
+    // setBlogs(blogs.map((b) => (b.id === blog.id ? updatedBlog : b)))
   }
 
   const remove = async (blog) => {
@@ -74,7 +86,7 @@ const App = () => {
     if (ok) {
       await blogService.remove(blog.id)
       notifyWith(`The blog' ${blog.title}' by '${blog.author} removed`)
-      setBlogs(blogs.filter((b) => b.id !== blog.id))
+      // setBlogs(blogs.filter((b) => b.id !== blog.id))
     }
   }
 
@@ -90,6 +102,17 @@ const App = () => {
 
   const byLikes = (b1, b2) => b2.likes - b1.likes
 
+  if (result.isLoading) {
+    return <div>loading data...</div>
+  }
+
+  if (result.isError) {
+    return <div>Something went wrong, please try again later...</div>
+  }
+
+  const queryBlogs = result.data
+  console.log('queryBlogs', queryBlogs)
+
   return (
     <div>
       <h2>blogs</h2>
@@ -102,7 +125,7 @@ const App = () => {
         <NewBlog createBlog={createBlog} />
       </Togglable>
       <div>
-        {blogs.sort(byLikes).map((blog) => (
+        {queryBlogs.sort(byLikes).map((blog) => (
           <Blog
             key={blog.id}
             blog={blog}
