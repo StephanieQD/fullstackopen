e3aef80993d31f6d52235b9c3b3b2698269907fa
StephanieQD <stephanie.qrys.deetman@gmail.com>
2023-11-06 13:26:27 -0600

    Complete exercise 8.2: All books
    Implement query allBooks, which returns the details of all books.

--- part8/library-backend/library-backend.js
+++ part8/library-backend/library-backend.js
@@ -91,9 +91,17 @@ let books = [
 */
 
 const typeDefs = `
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+    genres: [String!]!,
+  }
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `;
 
@@ -101,6 +109,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
+    allBooks: () => books,
   },
 };
 
