86b859990d7aaa9090b882556d8663d0a83e9ca3
StephanieQD <stephanie.qrys.deetman@gmail.com>
2023-11-17 16:43:15 -0600

    Complete exercise 8.19 Books by genre, part 1

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,17 +1,31 @@
 import { useQuery } from "@apollo/client";
 import { ALL_BOOKS } from "../queries";
+import { useState } from "react";
 
 const Books = () => {
+  const [genreFilter, setGenreFilter] = useState(null);
   const result = useQuery(ALL_BOOKS);
   if (result.loading) {
     return <div>loading...</div>;
   }
   const books = result.data.allBooks;
+  let genres = [];
+
+  // Get a list of all genres for filter buttons
+  for (let index = 0; index < books.length; index++) {
+    const book = books[index];
+    genres.push(...book.genres);
+  }
+  genres = [...new Set(genres)]; // Only unique values
 
   return (
     <div>
       <h2>books</h2>
-
+      {genreFilter && (
+        <p>
+          in genre <b>{genreFilter}</b>
+        </p>
+      )}
       <table>
         <tbody>
           <tr>
@@ -19,15 +33,27 @@ const Books = () => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
+          {books
+            .filter((book) =>
+              genreFilter ? book.genres.includes(genreFilter) : book
+            )
+            .map((book) => {
+              return (
+                <tr key={book.title}>
+                  <td>{book.title}</td>
+                  <td>{book.author.name}</td>
+                  <td>{book.published}</td>
+                </tr>
+              );
+            })}
         </tbody>
       </table>
+      {genres.map((g) => (
+        <button key={`genre-${g}`} onClick={() => setGenreFilter(g)}>
+          {g}
+        </button>
+      ))}
+      <button onClick={() => setGenreFilter(null)}>all genres</button>
     </div>
   );
 };
